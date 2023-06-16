3fbef6e13bd2f58adcbfc526163e89f616b7f72c
Alexey Vlasov <dreamfall13@mail.ru>
2023-06-16 15:13:49 +0300

    feat: implement create file function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,30 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { writeFile, access } from 'fs/promises';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pathToFile = join(__dirname, 'files/fresh.txt');
+const content = 'I am fresh and young';
+
+// Function Asynchronously Check if a File Exists
+// https://futurestud.io/tutorials/node-js-check-if-a-file-exists
+async function exists(path) {
+  try {
+    await access(path);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
 const create = async () => {
-  // Write your code here
+  try {
+    const isExists = await exists(pathToFile);
+    if (isExists) throw new Error('FS operation failed');
+    await writeFile(pathToFile, content, { flag: 'wx' });
+  } catch (e) {
+    console.error(e.message);
+  }
 };
 
 await create();

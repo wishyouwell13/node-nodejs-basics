3ac1b740a84f53b4f00e68821b49cc48a8a805d8
Alexey Vlasov <dreamfall13@mail.ru>
2023-06-16 20:21:39 +0300

    feat: implement function that parses environment variables

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  const result = [];
+  for (const [envKey, envValue] of Object.entries(process.env)) {
+    if (/^RSS_/.test(envKey)) {
+      const str = `${envKey}=${envValue}`;
+      result.push(str);
+    }
+  }
+  console.log(result.join('; '));
 };
 
 parseEnv();

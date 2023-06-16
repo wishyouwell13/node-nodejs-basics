09d1225948425a94a17d4087b806a8f4cab167dd
Alexey Vlasov <dreamfall13@mail.ru>
2023-06-16 15:32:43 +0300

    feat: implement copy folder function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,33 @@
-const copy = async () => {
-    // Write your code here 
+import { cp, access } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const path = join(__dirname, 'files');
+const copyPath = join(__dirname, 'files_copy');
+
+async function exists(path) {
+  try {
+    await access(path);
+    return true;
+  } catch {
+    return false;
+  }
+}
+const copy = async (src = path, dest = copyPath) => {
+  try {
+    const isDestExists = await exists(dest);
+    const isSrcExists = await exists(src);
+
+    if (isDestExists || !isSrcExists) throw new Error('FS operation failed 2');
+
+    await cp(path, dest, {
+      recursive: true,
+      force: false,
+    });
+  } catch (e) {
+    console.error(e.message);
+  }
 };
 
 await copy();

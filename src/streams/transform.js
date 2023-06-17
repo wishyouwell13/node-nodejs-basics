567fdd27b0ae55e1702147ec84a8e8468e9fd6be
Alexey Vlasov <dreamfall13@mail.ru>
2023-06-17 18:18:20 +0300

    feat: implement transform output string  with streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,24 @@
+import { Transform } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+
+const readable = process.stdin;
+const writable = process.stdout;
+
+const transformStream = new Transform({
+  transform(chunk, enc, cb) {
+    const chunkStringified = chunk.toString().trim();
+    const reversedChunk = chunkStringified.split('').reverse().join('');
+    this.push(reversedChunk + '\n');
+    cb();
+  },
+});
+
 const transform = async () => {
-    // Write your code here 
+  try {
+    await pipeline(readable, transformStream, writable);
+  } catch (err) {
+    console.err(err.message);
+  }
 };
 
 await transform();

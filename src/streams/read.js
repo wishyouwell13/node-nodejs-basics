f6404aca68cfe25f73548d4a201a70720b17a702
Alexey Vlasov <dreamfall13@mail.ru>
2023-06-17 15:55:31 +0300

    feat: implement file reading with stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+// https://nodejs.org/dist/latest-v18.x/docs/api/stream.html#streampipelinesource-transforms-destination-options
+import { pipeline } from 'node:stream/promises';
+import { createReadStream } from 'node:fs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  await pipeline(createReadStream(filePath), process.stdout);
 };
 
 await read();

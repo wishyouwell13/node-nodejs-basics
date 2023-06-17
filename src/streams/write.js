2cb02d661e965a3568c29d390392154490a1b6bd
Alexey Vlasov <dreamfall13@mail.ru>
2023-06-17 16:46:07 +0300

    feat: implement write data to file with stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+// https://nodejs.org/dist/latest-v18.x/docs/api/stream.html#streampipelinesource-transforms-destination-options
+import { pipeline } from 'node:stream/promises';
+import { createWriteStream } from 'node:fs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filePath = join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+  const stream = createWriteStream(filePath, { flags: 'w' });
+  //   process.stdin.pipe(stream);
+  await pipeline(process.stdin, stream);
 };
 
 await write();

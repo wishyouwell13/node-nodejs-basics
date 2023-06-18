e199a776c82d396fab582fcb65f3dc668b87b695
Alexey Vlasov <dreamfall13@mail.ru>
2023-06-18 22:54:24 +0300

    feat: implement task about creating child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import { fork } from 'node:child_process';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { pipeline } from 'stream/promises';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filePath = join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  fork(filePath, args, { stdio: [0, 1, 2, 'ipc'] });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['1', '23', 'tt']);
